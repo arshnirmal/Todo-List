7d59f8e8d9da357e5b25da47da94786590a7c0e6
arshnirmal <arshnirmal11@gmail.com>
2023-01-18 04:13:51 +0530

    signup_page

--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -11,11 +11,38 @@ class _SignupPageState extends State<SignupPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Signup"),
-      ),
-      body: const Center(
-        child: Text("Signup Page"),
+      backgroundColor: const Color.fromARGB(255, 16, 22, 57),
+      body: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.only(left: 32, right: 32, top: 32),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              const Image(
+                image: AssetImage("assets/images/landingPage3.png"),
+                width: 300,
+                height: 300,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 500,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
